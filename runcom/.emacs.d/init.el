f87783d3a87dab6846de82a05a69eaa91b4b720d
Rob Williams <jayrobwilliams@gmail.com>
2022-05-16 08:53:24 -0400

    update init.el

--- runcom/.emacs.d/init.el
+++ runcom/.emacs.d/init.el
@@ -1,11 +1,17 @@
 ;;; emacs config file
 
-;; line numbers
-(global-display-line-numbers-mode)
+;; Line and column numbers
+(column-number-mode)
+(global-display-line-numbers-mode t)
 
-;; add MELPA repo for flycheck
+;; Disable line numbers for some modes
+(dolist (mode '(org-mode-hook
+                term-mode-hook
+                eshell-mode-hook))
+  (add-hook mode (lambda () (display-line-numbers-mode 0))))
+
+;; add MELPA
 (require 'package)
-
 (add-to-list 'package-archives
              '("MELPA Stable" . "https://stable.melpa.org/packages/") t)
 (package-initialize)
@@ -22,10 +28,30 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(flycheck use-package)))
+ '(custom-enabled-themes '(sanityinc-tomorrow-day))
+ '(custom-safe-themes
+   '("bb08c73af94ee74453c90422485b29e5643b73b05e8de029a6909af6a3fb3f58" default))
+ '(package-selected-packages
+   '(color-theme-sanityinc-tomorrow rainbow-delimiters lsp-mode treemacs-tab-bar treemacs-persp treemacs-magit treemacs-icons-dired treemacs-projectile treemacs-evil winum pfuture treemacs flycheck use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+
+;; Rainbow delimiters
+(use-package rainbow-delimiters
+  :hook (prog-mode . rainbow-delimiters-mode))
+
+;; Tomorrow theme
+(use-package color-theme-sanityinc-tomorrow
+  :ensure t
+  :load-path "themes"
+  :init
+  (setq color-theme-sanityinc-tomorrow t)
+  :config
+  (load-theme 'sanityinc-tomorrow-day t)
+  )
+
+;;(load-theme 'sanityinc-tomorrow-day t)
