05ff139f3c55206dcd3c4dfb3feed954d04b0958
Rob Williams <jayrobwilliams@gmail.com>
2023-04-09 14:56:07 -0400

    simplify

--- macos/dock.sh
+++ macos/dock.sh
@@ -2,13 +2,13 @@
 
 # Applications
 dockutil --no-restart --remove all
-dockutil --no-restart --add "/Applications/Mail.app"
 dockutil --no-restart --add "/Applications/Google Chrome.app"
+dockutil --no-restart --add "/Applications/Firefox.app"
 dockutil --no-restart --add "/Applications/Zotero.app"
 dockutil --no-restart --add "/Applications/PDF Expert.app"
-dockutil --no-restart --add "/Applications/Rstudio.app"
+#dockutil --no-restart --add "/Applications/Rstudio.app"
 dockutil --no-restart --add "$(brew info emacs-plus | sed -n 2p | cut -d " " -f1)/Emacs.app"
-dockutil --no-restart --add "/Applications/texstudio.app"
+#dockutil --no-restart --add "/Applications/texstudio.app"
 dockutil --no-restart --add "/Applications/System Preferences.app"
 
 # Folders
